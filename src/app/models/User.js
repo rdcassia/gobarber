128c82548aa381e83c795a4d999f4e32b4b0e12f
cassia <rdcassia.alves@gmail.com>
2020-01-31 14:56:55 -0300

    Auth

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -26,7 +26,13 @@ class User extends Model {
 
 		return this;
 
-  }
+	}
+
+	checkPassword(password) {
+		return bcrypt.compare( password, this.password_hash );
+	}
+
+
 }
 
 export default User;
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+	secret: '6B923E3ED758B66F9531740C440F5D83',
+	expiresIn: '7d',
+};
