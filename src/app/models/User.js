a583a2caf37ee06aa8ef18439307ed2de06b6502
cassia <rdcassia.alves@gmail.com>
2020-01-31 12:55:16 -0300

    UserController

---
+++ src/app/models/User.js
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import Sequelize, { Model } from 'sequelize';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init({
+      name: Sequelize.STRING,
+      email: Sequelize.STRING,
+      password_hash: Sequelize.STRING,
+      provider: Sequelize.BOOLEAN,
+		},
+		{
+      sequelize,
+		}
+	);
+  }
+}
+
+export default User;
