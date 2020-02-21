3f27fc5b8215169572df93e92e5f7f7791e1d88f
rdcassia.alves <rdcassia.alves@gmail.com>
2020-02-20 22:10:32 -0300

    Environment Variables

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '4c8b0b704be665',
-    pass: 'a8c675f1fa432b',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
